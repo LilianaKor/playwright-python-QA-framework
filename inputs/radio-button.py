158cc1b8379241fdb71529f1d58677476d85882a
Liliana <lilianakoric@gmail.com>
2024-06-21 22:00:53 -0700

    Add python3 inputs/options.py, radio-button.py

--- inputs/radio-button.py
+++ inputs/radio-button.py
@@ -1,6 +1,7 @@
-#to run this: python3 inputs/radio-button.py
+# to run this: python3 inputs/radio-button.py
 import asyncio
 from playwright.async_api import async_playwright, expect
+
 
 async def main():
     async with async_playwright() as p:
@@ -10,15 +11,16 @@ async def main():
         page = await context.new_page()
         await page.set_viewport_size({"width": 1800, "height": 1200})
         await page.goto("https://demoqa.com/radio-button")
-        #-Actions
+        # -Actions
         await page.check("#yesRadio", force=True)
         await page.screenshot(path="screenshots/radioButton.png")
-        #-Assertions
+        # -Assertions
         await page.is_checked("#yesRadio") is True
         await expect(page.locator(".text-success")).to_have_text("Yes")
-        #-Stoping Tracing
-        await context.tracing.stop(path = "logs/trace.zip")
-        #-Closing browser
+        # -Stoping Tracing
+        await context.tracing.stop(path="logs/trace.zip")
+        # -Closing browser
         await browser.close()
+
 
 asyncio.run(main())
